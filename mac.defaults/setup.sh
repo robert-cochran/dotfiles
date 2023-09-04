06ba4fce20143c68422b0731eec8e83387114f73
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2023-09-05 07:21:52 +1000

    bkp

--- mac.defaults/setup.sh
+++ mac.defaults/setup.sh
@@ -12,7 +12,8 @@ killall Finder
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
+# NOTE: 'disablelocal' now unrecognized verb, returns error 
+#hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 # "Set a blazingly fast keyboard repeat rate"
 # default was 2, custom setting suggested 0.02 but thats dumb fast
