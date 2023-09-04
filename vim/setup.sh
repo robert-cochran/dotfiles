5ecf62b344d6f5739ef38e0b79837fdfb0c03cde
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2023-09-05 06:59:24 +1000

    bkp

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,19 +2,38 @@
 
 local DOTFILES_DIR=${HOME}/dotfiles
 
-# NOTE: mac comes preinstalled with vim. to upgrade vim either compile it to 
-# /usr/bin/vim, or install/upgrade with brew, or use neovim
+if ! command -v brew > /dev/null; then
 
-# symlink .vimrc to home dir
-ln -sf ${DOTFILES_DIR}/vim/.vimrc ~/.vimrc
+    # NOTE: mac comes preinstalled with vim. to upgrade vim either compile 
+    # it to /usr/bin/vim, or install/upgrade with brew, (or use neovim)
+    brew install vim
 
-# symlink .vim/ to home dir
-# note: using target dir ~/.vim doesnt work, creates nested .vim in ~
-# but the same ln command is used for both zsh and vim setup.sh 
-# why does this ln not work when using the same format for 
-# the other dirs .zsh_alias and .vim
-# ln -sf ~/dotfiles/vim/.vim ~/.vim
-ln -sf ${DOTFILES_DIR}/vim/.vim ~/
+fi
 
-# symlink zsh aliases into zsh_alias
-ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
+if ! command -v vim > /dev/null; then
+
+    # TODO install vim plugin manager
+    # otherwise plugins in vimrc will cause errors when zsh starts
+    
+    # symlink .vimrc to home dir
+    ln -sf ${DOTFILES_DIR}/vim/.vimrc ~/.vimrc
+
+    # symlink .vim/ to home dir
+    # note: using target dir ~/.vim doesnt work, creates nested .vim in ~
+    # but the same ln command is used for both zsh and vim setup.sh 
+    # why does this ln not work when using the same format for 
+    # the other dirs .zsh_alias and .vim
+    ln -sf ${DOTFILES_DIR}/vim/.vim ~/
+
+    # symlink zsh aliases into zsh_alias
+    # ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
+    # cp ${DOTFILES_DIR}/vim/.vim.zshrc
+
+fi
+
+
+
+
+
+
+
