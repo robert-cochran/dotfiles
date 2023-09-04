a3c27b531800c8a230736599b270cceaec37ceb2
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2023-09-05 07:21:26 +1000

    bkp

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,7 +10,8 @@ if ! command -v brew > /dev/null; then
 
 fi
 
-if ! command -v vim > /dev/null; then
+# NOTE: if command not working here for some reason
+#if ! command -v vim > /dev/null; then
 
     # TODO install vim plugin manager
     # otherwise plugins in vimrc will cause errors when zsh starts
@@ -19,17 +20,13 @@ if ! command -v vim > /dev/null; then
     ln -sf ${DOTFILES_DIR}/vim/.vimrc ~/.vimrc
 
     # symlink .vim/ to home dir
-    # note: using target dir ~/.vim doesnt work, creates nested .vim in ~
-    # but the same ln command is used for both zsh and vim setup.sh 
-    # why does this ln not work when using the same format for 
-    # the other dirs .zsh_alias and .vim
     ln -sf ${DOTFILES_DIR}/vim/.vim ~/
 
     # symlink zsh aliases into zsh_alias
     # ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
     # cp ${DOTFILES_DIR}/vim/.vim.zshrc
 
-fi
+#fi
 
 
 
