3aa7a1b7249cdb640cb9c648e404a120178eef3f
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2023-09-06 20:06:26 +1000

    bkp

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,11 +4,6 @@ local DOTFILES_DIR=${HOME}/dotfiles
 
 echo "Vim setup.sh"
 
-# This check isn't right. 
-# Since vim comes preinstalled with mac it shouldn't check if vim is present,
-# it needs to check if vim has been installed by brew
-#if ! type vim > /dev/null ; then
-
 if ! type brew > /dev/null; then
 
     [ -f ${DOTFILES_DIR}/brew/setup.sh ] && \
@@ -30,17 +25,15 @@ fi
 # otherwise plugins in vimrc will cause errors when zsh starts
 
 echo "[LINK] linking .vimrc to ~"
-ln -sf ${DOTFILES_DIR}/vim/.vimrc ~/.vimrc
+ln -sf ${DOTFILES_DIR}/vim/.vimrc ${HOME}/.vimrc
+
 
 echo "[LINK] linking .vim to ~"
-ln -sf ${DOTFILES_DIR}/vim/.vim ~/
+ln -sf ${DOTFILES_DIR}/vim/.vim ${HOME}
 
 # TODO symlink zsh aliases into zsh_alias
 echo "[LINK] linking .vim.zshrc to ~/.zsh_alias"
-ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
-
-#echo "copying .vim.zshrc to ~'s .zsh_alias"
-#cp ${DOTFILES_DIR}/vim/.vim.zshrc ${HOME}/.zsh_alias
+ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ${HOME}/zsh_alias
 
 echo "Vim setup finished"
 
