470d2f44eb9e3f7a4c26cc727bff38f64d60a549
robert-cochran <4916242+robert-cochran@users.noreply.github.com>
2023-09-06 19:56:25 +1000

    bkp

--- vim/setup.sh
+++ vim/setup.sh
@@ -21,33 +21,26 @@ if ! brew list | grep vim > /dev/null; then
 
     # NOTE: mac comes preinstalled with vim. to upgrade vim either compile 
     # it to /usr/bin/vim, or install/upgrade with brew, (or use neovim)
-    echo "[SETUP] Installing vim via brew"
+    echo "[INSTALL] Installing vim via brew"
     brew install vim
-
-fi
-
-
-if  brew list | grep vim > /dev/null; then
-
-    echo "vim already installed by brew"
 
 fi
 
 # TODO install vim plugin manager
 # otherwise plugins in vimrc will cause errors when zsh starts
 
-echo "[SETUP] linking .vimrc to ~"
+echo "[LN] linking .vimrc to ~"
 ln -sf ${DOTFILES_DIR}/vim/.vimrc ~/.vimrc
 
-echo "[SETUP] linking .vim to ~"
+echo "[LN] linking .vim to ~"
 ln -sf ${DOTFILES_DIR}/vim/.vim ~/
 
 # TODO symlink zsh aliases into zsh_alias
-#echo "[SETUP] linking .vim.zshrc to ~/.zsh_alias"
-#ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
+echo "[LN] linking .vim.zshrc to ~/.zsh_alias"
+ln -sf ${DOTFILES_DIR}/vim/.vim.zshrc ~/.zsh_alias
 
 #echo "copying .vim.zshrc to ~'s .zsh_alias"
-cp ${DOTFILES_DIR}/vim/.vim.zshrc ${HOME}/.zsh_alias
+#cp ${DOTFILES_DIR}/vim/.vim.zshrc ${HOME}/.zsh_alias
 
 echo "Vim setup finished"
 
